2287c70afb2732e3e08c311f3740b30ccfafbbc6
bukit3point0 <bukit3point0@gmail.com>
2021-04-25 22:40:01 -0400

    edited home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,29 +8,29 @@ import recordingEquipment from '../images/recording-equipment.jpg'
 
 const Page = styled.div`
     background-color: black;
-    height: 100vh;
     color: white;
     font-family: PressStart2P;
+    padding: 3rem 0 1rem;
 `
 const ImageDiv = styled.div`
     display: flex;
     width: 80%;
     justify-content: space-between;
     margin: auto;
-    padding-top: 3rem;
+    
 `
 const Image = styled.img`
     width: 20%;
     border: .75rem solid white;
 `
 const Header = styled.h1`
-    margin: 3rem 0;
+    margin: 1.5rem 0;
     text-align: center;
     font-size: 5rem;
 `
 const LoginSignUp = styled.div`
     display: flex;
-    margin: auto;
+    margin: 0 auto 1.5rem;
     justify-content: center;
 `
 const Buttons = styled.button`
@@ -41,6 +41,25 @@ const Buttons = styled.button`
     font-size: 2rem;
     padding: 1rem;
     border-radius: 20px;
+`
+const TagLine = styled.h2`
+    font-size: 1.5rem;
+    width: 80%;
+    text-align: center;
+    margin: 1rem auto 1.5rem;
+`
+const InfoDiv = styled.div`
+    width: 80%;
+    border: .25rem solid white;
+    margin: 1rem auto;
+    padding: 1rem;
+`
+const InfoTitle = styled.h3`
+    font-size: 1.5rem;
+`
+const Info = styled.p`
+    padding: .5rem 0;
+    text-align: justify;
 `
 
 const Home = () => {
@@ -68,6 +87,16 @@ const Home = () => {
                 <Buttons onClick = {login}>Log In</Buttons>
                 <Buttons onClick = {signup}>Sign Up</Buttons>
             </LoginSignUp>
+            <TagLine>Tired fo paying ridiculous fees for camera and other equipment rentals? Bypass the middleman and rent from a real person!</TagLine>
+            <InfoDiv>
+                <InfoTitle>What Is "Use My Tech Stuff"?</InfoTitle>
+                <Info>
+                    Tech stuff is EXPENSIVE. Tech stuff that's only needed once or twice isn't worth the cost. Talk to equipment owners directly and get the tech you need faster.
+                </Info>
+                <Info>
+                    We cut out the middleman. That means your rentals are cheaper and the owners keep more of the profits! Rent for a day, a week, even a month or more - extend your rental at any time.
+                </Info>
+            </InfoDiv>
         </Page>
     )
 }
