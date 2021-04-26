db704a8db71f5b8b25857576f91d9d2cf1f840ea
drslaughterrex <drslaughterrex@gmail.com>
2021-04-26 13:47:04 -0500

    Initial push

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,98 @@
-import styled from 'styled-components'
-import {useHistory} from 'react-router-dom'
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
 
-const Page = styled.div``
+const Page = styled.div``;
+
+const initialFormValues = {
+	username: "",
+	password: "",
+};
+
+const initialFormErrors = {
+	username: "Please enter a valid username.",
+	password: "Your password was incorrect.",
+};
 
 const Login = () => {
+	// /login
+	// Accept a username/email and password
+	// Route to protectedroute after token is received
+	// form validation
 
-    // /login
-    // Accept a username/email and password
-    // Route to protectedroute after token is received
-    // form validation
-    
-    return(
-        <Page>
+	const [username, setUsername] = useState([]);
+	const [formValues, setFormValues] = useState(initialFormValues);
+	const [formErrors, setFormErrors] = useState(initialFormErrors);
+	const [submitDisabled, setSubmitDisabled] = useState(true);
 
-        </Page>
-    )
-}
+	const onChangeHandler = (username, value) => {
+		yup
+			.reach(schema, username)
+			.validate(value)
+			.then(() => {
+				setFormErrors({
+					...formErrors,
+					[username]: " ",
+				});
+			})
+			.catch((err) => {
+				setFormErrors({
+					...formErrors,
+					[username]: err.errors[0],
+				});
+			});
+		setFormValues({ ...formValues, [username]: value });
+	};
 
-export default Login
+	const submitHandler = (e) => {
+		const newUser = {
+			username: formValues.username.trim(),
+			email: formValues.email.trim(),
+		};
+		setUsername([...username, newUser]);
+	};
+
+	useEffect(() => {
+		schema.isValid(formValues).then((valid) => {
+			setSubmitDisabled(!valid);
+		});
+	}, [formValues]);
+
+    const onSubmit = (e) => {
+		e.preventDefault();
+		submitHandler();
+	};
+
+
+	return <Page>
+        <div>
+            <form onSubmit={onSubmit}>
+                <label className='name'>
+                    {' '}
+                    Username:
+                    <input
+                    type="text"
+                    name='username'
+                    value={formValues.username}
+                    onChange={onChange}
+                    />
+                </label>
+                <label className='password'>
+					{" "}
+					Password:
+					<input
+						type="password"
+						name="password"
+						value={formValues.password}
+						onChange={onChange}
+					/>
+				</label>
+
+                <button disabled={submitDisabled}> Submit </button>
+            </form>
+        </div>
+    </Page>;
+};
+
+export default Login;
