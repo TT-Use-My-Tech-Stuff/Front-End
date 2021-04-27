cf8bfd08dbf741e320d99dae3e63a2612f8bc7a0
Johnny Murillo <jossters@gmail.com>
2021-04-27 01:30:54 -0700

    Johnny Murillo 130am

--- src/components/Owners/CreateListing.js
+++ src/components/Owners/CreateListing.js
@@ -1,20 +1,74 @@
-import styled from 'styled-components'
-import {useHistory} from 'react-router-dom'
+import styled from "styled-components";
+import { Link, useHistory } from "react-router-dom";
+import { axiosWithAuth } from "../axiosWithAuth";
+import { useState } from "react";
 
-const Page = styled.div``
+const Page = styled.div``;
 
 const CreateListing = () => {
+  // /owner/create-listing
+  // allow only profiles that are owners
+  // create a new object for rent that will be displayed on the owners' home page
+  // form validation
 
-    // /owner/create-listing
-    // allow only profiles that are owners
-    // create a new object for rent that will be displayed on the owners' home page
-    // form validation
-    
-    return(
-        <Page>
+  const { push } = useHistory();
+  const [item, setItem] = useState({
+    itemName: "Test",
+    ownerName:"Test",
+  });
 
-        </Page>
-    )
+  const handleChange = (e) => {
+    setItem({
+        ...item,
+        [e.target.name]: e.target.value
+    });
 }
 
-export default CreateListing
+const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth
+    .post(`/api/equipment/createEquipment/:id`, item)
+    .then((res) => {
+      console.log('create')
+      push(`/owner/:id`);
+    })
+    .catch((err) => console.log({ err }));
+}
+
+const { itemName, ownerName} = item;
+
+  return (
+    <Page>
+      <h1>Create Listing </h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          
+          <div>
+          <label>Item Name: </label>
+          <input
+            value={itemName}
+            onChange={handleChange}
+            name="itemname"
+            type="text"
+          />
+          </div>
+          <br></br>
+          <div>
+          <label>Owner Name: </label>
+          <input
+            value={ownerName}
+            onChange={handleChange}
+            name="ownername"
+            type="text"
+          />
+          </div>
+        </div>
+        <input type="submit" className="inputbutton" value="Save"/>
+        <Link to={"/owner/:id"}><input type="button" className="inputbutton" value="Cancel"/></Link>
+      </form>
+      
+    </Page>
+  );
+};
+
+export default CreateListing;
--- src/components/Owners/EditListing.js
+++ src/components/Owners/EditListing.js
@@ -1,20 +1,81 @@
-import styled from 'styled-components'
-import {useHistory} from 'react-router-dom'
+import styled from "styled-components";
+import { Link, useHistory } from "react-router-dom";
+import { useState } from "react";
+import { axiosWithAuth } from "../axiosWithAuth";
+import { useEffect } from "react";
 
-const Page = styled.div``
+const Page = styled.div``;
 
 const EditListing = () => {
+  // /owner/edit-listing
+  // allow only profiles that are owners
+  // edit a current item for rent that will be displayed on the owners' home page
+  // form validation
 
-    // /owner/edit-listing
-    // allow only profiles that are owners
-    // edit a current item for rent that will be displayed on the owners' home page
-    // form validation
-    
-    return(
-        <Page>
+  const { push } = useHistory();
+  const [item, setItem] = useState({
+    itemName: "Test",
+    ownerName:"Test",
+  });
 
-        </Page>
-    )
-}
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/api/equipment/owner/:id")
+      .then((res) => console.log(res))
+      .catch((err) => console.log({ err }));
+  }, []);
 
-export default EditListing
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth
+      .post(`/api/equipment/createEquipment/:id`, item)
+      .then((res) => {
+        console.log("create");
+        push(`/owner/:id`);
+      })
+      .catch((err) => console.log({ err }));
+  };
+
+  const handleChange = (e) => {
+    setItem({
+      ...item,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { itemName, ownerName} = item;
+
+  return (
+    <Page>
+      <h1>Edit Listing </h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          
+          <div>
+          <label>Item Name: </label>
+          <input
+            value={itemName}
+            onChange={handleChange}
+            name="itemname"
+            type="text"
+          />
+          </div>
+          <br></br>
+          <div>
+          <label>Owner Name: </label>
+          <input
+            value={ownerName}
+            onChange={handleChange}
+            name="ownername"
+            type="text"
+          />
+          </div>
+        </div>
+        <input type="submit" className="inputbutton" value="Save"/>
+        <Link to={"/owner/:id"}><input type="button" className="inputbutton" value="Cancel"/></Link>
+      </form>
+    </Page>
+  );
+};
+
+export default EditListing;
