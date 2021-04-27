dcd24ff8d85d84666f9f0d7f10a22b3bab8f801b
Craig Gerhardstein <77256669+Craig-Gerhardstein@users.noreply.github.com>
2021-04-27 13:07:40 -0400

    chart

--- src/components/Renters/Cart.js
+++ src/components/Renters/Cart.js
@@ -1,17 +1,78 @@
 import styled from 'styled-components'
-import {useHistory} from 'react-router-dom'
 
-const Page = styled.div``
+const cartData = [
+    {
+        name: "VCR1",
+        equipment_id: 1,
+        equipment_description: "This is a test",
+        owner_id: 2,
+       
+
+    },
+    {
+        name: "VCR2",
+        equipment_id: 2,
+        equipment_description: "This is a test",
+        owner_id: 2,
+        
+
+    },
+    {
+        name: "VCR3",
+        equipment_id: 3,
+        equipment_description: "This is a test",
+        owner_id: 2,
+        
+
+    }
+]
+
+const Page = styled.div`
+/* border: 2px solid black; */
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+div {
+    border: 1px solid black;
+    margin: 1%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 30%;
+}
+button {
+    width: 15%;
+    margin: 1%;
+    
+
+}
+p {
+    margin: 1%;
+}
+
+`
+
 
 const Cart = () => {
-
-    // /renter/cart
-    // allows the renter to see everything they are currently requesting to rent, but not things they are currently renting
-    // allows the renter to remove items from cart
-    // submit cart
+    
+    
     
     return(
         <Page>
+            {cartData.map(item => {
+                return (
+                    <div>
+                        <p>name: {item.name}</p>
+                        <p>description: {item.equipment_description}</p>
+                        <button>Remove</button>
+                         
+                    </div>
+                   
+                )
+            })}
 
         </Page>
     )
