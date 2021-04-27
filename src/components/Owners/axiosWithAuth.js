f3b54da64df62fd458fe11946d20a6dc1dd8fbdc
Vince Garcia <vinceryangarcia@gmail.com>
2021-04-27 15:29:34 -0700

    create listing functional and app js routing edited

---
+++ src/components/Owners/axiosWithAuth.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem('token')
+
+    return axios.create({
+        headers: {
+            authorization: token
+        },
+        baseURL: "https://back-end-tt.herokuapp.com/",
+    })
+}
+
+export default axiosWithAuth;
