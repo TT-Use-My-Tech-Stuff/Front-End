03b6ade448b9b2856c29f8bdfbbd74249579ac7f
bukit3point0 <bukit3point0@gmail.com>
2021-04-29 14:03:48 -0400

    changes login route

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import axiosWithAuth from './axiosWithAuth';
+import OwnerHome from './Owners/OwnerHome';
+import RenterHome from './Renters/RenterHome';
 
 // This is where you protect routes so users need a token to access
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
+const ProtectedRoute = () => {
 
-  return (<Route {...rest} render={
-    (props) => {
-      if (localStorage.getItem('token')) {
-        return <Component {...props} />; //returns to components if it gets the token
-      } else {
-        return <Redirect to='/login' />;  //redirects to login if it can't find a token
-      }
-    }}
-  />)
+  const userType = localStorage.getItem("type")
+  console.log(userType)
+
+  return(
+    <div>
+    {
+      userType === "owner"
+      ? <OwnerHome/> 
+      : <RenterHome/>
+    }
+    </div>
+  )
 }
+// const ProtectedRoute = ({ component: Component, ...rest }) => {
+
+//   return (<Route {...rest} render={
+//     (props) => {
+//       if (localStorage.getItem('token')) {
+//         return <Component {...props} />; //returns to components if it gets the token
+//       } else {
+//         return <Redirect to='/login' />;  //redirects to login if it can't find a token
+//       }
+//     }}
+//   />)
+// }
 
 export default ProtectedRoute;
